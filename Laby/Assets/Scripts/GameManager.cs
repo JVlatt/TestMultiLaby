5d215a42825938114590a75b95e1c60470d448f7
Théo <theo.boldireff@edu.devinci.fr>
2020-11-09 19:52:00 +0100

    Proto fonctionnel (Uniquement pour le player prefab de PUN)

--- Laby/Assets/Scripts/GameManager.cs
+++ Laby/Assets/Scripts/GameManager.cs
@@ -8,11 +8,41 @@ using Photon.Pun;
 using Photon.Realtime;
 
 
-namespace Com.DefaultCompany.Laby
+namespace Photon.Pun.Demo.PunBasics
 {
     public class GameManager : MonoBehaviourPunCallbacks
     {
+        #region Public Fields
 
+        [Tooltip("The prefab to use for representing the player")]
+        public GameObject playerPrefab;
+
+        #endregion
+
+        #region MonoBehaviour CallBacks
+
+        private void Start()
+        {
+            if (playerPrefab == null)
+            {
+                Debug.LogError("<Color=Red><a>Missing</a></Color> playerPrefab Reference. Please set it up in GameObject 'Game Manager'", this);
+            }
+            else
+            {
+                if (PlayerManager.LocalPlayerInstance == null)
+                {
+                    Debug.LogFormat("We are Instantiating LocalPlayer from {0}", SceneManagerHelper.ActiveSceneName);
+                    // we're in a room. spawn a character for the local player. it gets synced by using PhotonNetwork.Instantiate
+                    PhotonNetwork.Instantiate(this.playerPrefab.name, new Vector3(0f, 5f, 0f), Quaternion.identity, 0);
+                }
+                else
+                {
+                    Debug.LogFormat("Ignoring scene load for {0}", SceneManagerHelper.ActiveSceneName);
+                }
+            }
+        }
+
+        #endregion
 
         #region Photon Callbacks
 
