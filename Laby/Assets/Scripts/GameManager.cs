8ca4df0ac8e0800fc24c3e0eebdd57164c3e67aa
Théo <theo.boldireff@edu.devinci.fr>
2020-11-10 16:29:55 +0100

    Only 1 room for 5 players max

--- Laby/Assets/Scripts/GameManager.cs
+++ Laby/Assets/Scripts/GameManager.cs
@@ -94,8 +94,8 @@ namespace Photon.Pun.Demo.PunBasics
             {
                 Debug.LogError("PhotonNetwork : Trying to Load a level but we are not the master Client");
             }
-            Debug.LogFormat("PhotonNetwork : Loading Level : {0}", PhotonNetwork.CurrentRoom.PlayerCount);
-            PhotonNetwork.LoadLevel("Room for " + PhotonNetwork.CurrentRoom.PlayerCount);
+            //Debug.LogFormat("PhotonNetwork : Loading Level : {0}", PhotonNetwork.CurrentRoom.PlayerCount);
+            PhotonNetwork.LoadLevel("Room for 5");
         }
 
 
