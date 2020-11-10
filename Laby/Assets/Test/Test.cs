55d07d0c1923dd7915c189839556514ca6d1bb92
Théo <theo.boldireff@edu.devinci.fr>
2020-11-10 16:05:00 +0100

    ajout Test instanciation IRT

---
+++ Laby/Assets/Test/Test.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon;
+using Photon.Pun;
+
+    public class Test : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        public void Inst()
+        {
+            PhotonNetwork.Instantiate("prefab", new Vector3(0, 1, 0), Quaternion.identity, 0);
+        }
+    }
+
+
